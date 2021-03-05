86e35d65af865099b7bb640e34ca078d33d1f4a0
Juan Carlos Rabellino <juancarabellino@gmail.com>
2021-03-05 18:13:52 -0300

    First deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const express=require("express");
-const cors= require("cors");
-const router=require("./routes/index");
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/index");
 require("./config/database");
 var fileupload = require("express-fileupload");
 
@@ -11,8 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(fileupload());
 
-app.use("/api",router);
+app.use("/api", router);
 
-const port=process.env.PORT || 4000;
-const host=process.env.HOST || "0.0.0.0";
-app.listen(port,host,()=>console.log("App listening on port 4000"));
+const port = process.env.PORT || 4000;
+const host = process.env.HOST || "0.0.0.0";
+
+app.listen(port, host, () => console.log("App listening on port 4000"));
